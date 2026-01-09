52d8825458d77a414f82c841533efeead2fdf322
Sijal Manandhar <sijal.m06@gmail.com>
2026-01-09 19:14:37 +0545

    update favicon and styles, and implement useTheme composable

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ThemeToggler from "./components/ThemeToggler.vue";
+</script>
 
 <template>
+    <ThemeToggler />
     <h2>browser extension manager ui</h2>
 </template>
---
+++ src/composables/useTheme.ts
@@ -0,0 +1,44 @@
+import { ref, onMounted, onUnmounted, watch } from "vue";
+
+export default function useTheme() {
+  const STORAGE_KEY = "user-theme-preference";
+  const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+  const theme = ref(
+    localStorage.getItem(STORAGE_KEY) ||
+      (mediaQuery.matches ? "dark" : "light"),
+  );
+
+  const systemHandler = (event: MediaQueryListEvent) => {
+    if (!localStorage.getItem(STORAGE_KEY)) {
+      theme.value = event.matches ? "dark" : "light";
+    }
+  };
+
+  const toggleTheme = () => {
+    theme.value = theme.value === "dark" ? "light" : "dark";
+    localStorage.setItem(STORAGE_KEY, theme.value);
+  };
+
+  watch(
+    theme,
+    (newTheme) => {
+      if (newTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      } else {
+        document.documentElement.classList.remove("dark");
+      }
+    },
+    { immediate: true },
+  );
+
+  onMounted(() => {
+    mediaQuery.addEventListener("change", systemHandler);
+  });
+
+  onUnmounted(() => {
+    mediaQuery.removeEventListener("change", systemHandler);
+  });
+
+  return { theme, toggleTheme, isDark: ref(theme.value === "dark") };
+}
